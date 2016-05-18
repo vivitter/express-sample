6be9f246d326f33d05495275bc766afcff447a2c
vivitter <koji1989@gmail.com>
2016-05-19 00:59:47 +0900

    Added User Registration Functions.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // Get Users Documents
+  request.get('http://localhost:3000/users', function(error, response, body){
+    var docs = JSON.parse(response.body);
+    res.render('index', { title: 'Express', users: docs });
+  });
 });
 
+// Register new user
 router.post('/', function(req, res) {
-  console.log(req.body.title);
-  res.end('ok');
+  // Create new user object
+  var user = {
+    id: req.body.id,
+    password: req.body.password
+  };
+
+  // Request Option
+  var options = {
+    url: 'http://localhost:3000/users',
+    form: user,
+    json: true
+  }
+
+  // Post request to User API
+  request.post(options, function(error, response, body){
+    res.redirect('/');
+  })
 })
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
+// Definition of the user schema
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({
+  id: String,
+  password: String
+});
+var User = mongoose.model('User', UserSchema);
+
+// Connect to DB
+mongoose.connect('mongodb://localhost/tetsudb');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.find({}, function(err, docs){
+    if(err) {
+      res.end('error');
+    }
+
+    var users = [];
+    docs.forEach(function(doc){
+      users.push(doc);
+    });
+    res.json(users);
+  });
+});
+
+router.post('/', function(req, res) {
+  var new_user = req.body;
+  console.log(new_user);
+
+  var user = new User();
+  user.id = new_user.id;
+  user.password = new_user.password;
+  user.save(function(err){
+    if(err){
+      console.log(err);
+    }
+  })
+  res.end('ok');
 });
 
 module.exports = router;
